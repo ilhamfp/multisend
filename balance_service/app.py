f0369fd4dcdaf88701fb24cc6da747ec917e1149
Gabriel <yukinotenshi>
2018-11-15 19:37:46 +0700

    added balance service

---
+++ balance_service/app.py
@@ -0,0 +1,58 @@
+from flask import Flask, jsonify, request
+import requests
+
+from balance_service.model import *
+
+
+auth_broker_url = 'http://127.0.0.1:9999/auth'
+balance_app = Flask(__name__)
+
+
+@balance_app.before_request
+def _db_connect():
+    if db.is_closed():
+        db.connect()
+
+
+@balance_app.teardown_request
+def _db_close(exc):
+    if not db.is_closed():
+        db.close()
+
+
+@balance_app.route('/', methods=['GET'])
+def get_balance():
+    response = requests.get(auth_broker_url, headers = request.headers).json()
+    if response.get('error'):
+        return jsonify({
+            'error': 'User not found'
+        })
+
+    balance = Balance.get_or_none(Balance.user_id == response.get('id'))
+
+    if balance is None:
+        return jsonify({
+            'error': 'Balance for that user is not found'
+        })
+
+    return jsonify(balance.serialize())
+
+
+@balance_app.route('/', methods=['POST'])
+def create_balance():
+    response = requests.get(auth_broker_url, headers=request.headers).json()
+    if response.get('error'):
+        return jsonify({
+            'error': 'User not found'
+        })
+
+    balance = Balance.get_or_none(Balance.user_id == response.get('id'))
+    if balance:
+        return jsonify({
+            'error': 'Balance instance already exists'
+        })
+
+    balance = Balance(user_id=response.get('id'))
+    balance.save()
+
+    return jsonify(balance.serialize())
---
+++ balance_service/model.py
@@ -0,0 +1,28 @@
+from playhouse.shortcuts import model_to_dict, dict_to_model
+from peewee import *
+from datetime import datetime
+
+db = SqliteDatabase('balance_service/data.db')
+
+
+class BaseModel(Model):
+    updated_at = DateTimeField(default=datetime.now)
+    created_at = DateTimeField(default=datetime.now)
+
+    def save(self, *args, **kwargs):
+        self.updated_at = datetime.now()
+        return super(BaseModel, self).save(*args, **kwargs)
+
+    def serialize(self):
+        return model_to_dict(self, backrefs=True, recurse=False)
+
+    class Meta:
+        database = db
+
+
+class Balance(BaseModel):
+    user_id = IntegerField()
+    balance = IntegerField(default=0)
+
+    class Meta:
+        db_table = 'balance'
