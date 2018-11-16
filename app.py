6aaea2f502a40bd313c11f36f04f6f8c06554b3c
Gabriel <yukinotenshi>
2018-11-16 16:14:54 +0700

    added customer service

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 
 from authentication_broker.app import *
 from balance_service.app import *
+from customer_service.app import *
 
 
 app = Flask(__name__)
@@ -12,6 +13,9 @@ auth_broker_thread = threading.Thread(target=auth_app.run, args=[], kwargs={'hos
 
 balance_service_url = 'http://127.0.0.1:5001/'
 balance_service_thread = threading.Thread(target=balance_app.run, args=[], kwargs={'host': '127.0.0.1', 'port': 5001})
+
+customer_service_url = 'http://127.0.0.1:5002'
+customer_service_thread = threading.Thread(target=customer_app.run, args=[], kwargs={'host': '127.0.0.1', 'port': 5002})
 
 
 @app.route('/auth', methods=['GET', 'POST'])
@@ -32,7 +36,17 @@ def balance_proxy(varargs=''):
         return requests.post(balance_service_url + varargs, headers=request.headers, data=request.form).text
 
 
+@app.route('/customer', methods=['GET', 'POST'])
+@app.route('/customer/<path:varargs>', methods=['GET', 'POST'])
+def customer_proxy(varargs=''):
+    if request.method == 'GET':
+        return requests.get(customer_service_url + varargs, headers=request.headers, params=request.args).text
+    else:
+        return requests.post(customer_service_url + varargs, headers=request.headers, data=request.form).text
+
+
 if __name__ == '__main__':
     auth_broker_thread.start()
     balance_service_thread.start()
+    customer_service_thread.start()
     app.run(host='127.0.0.1', port=9999)
---
+++ customer_service/app.py
@@ -0,0 +1,65 @@
+from flask import Flask, jsonify, request
+import requests
+
+from customer_service.model import *
+
+
+auth_broker_url = 'http://127.0.0.1:5000/'
+customer_app = Flask(__name__)
+
+
+@customer_app.before_request
+def _db_connect():
+    if db.is_closed():
+        db.connect()
+
+
+@customer_app.teardown_request
+def _db_close(exc):
+    if not db.is_closed():
+        db.close()
+
+
+@customer_app.route('/', methods=['GET'])
+def get_customer():
+    response = requests.get(auth_broker_url, headers=request.headers).json()
+    if response.get('error'):
+        return jsonify({
+            'error': 'User not found'
+        })
+
+    customer = Customer.get_or_none(Customer.user_id == response.get('id'))
+
+    if customer is None:
+        return jsonify({
+            'error': 'Customer for that user is not found'
+        })
+
+    return jsonify(customer.serialize())
+
+
+@customer_app.route('/', methods=['POST'])
+def register():
+    response = requests.post(auth_broker_url, headers=request.headers, data=request.form).json()
+    if response.get('error'):
+        return jsonify({
+            'error': 'Fail creating customer account'
+        })
+
+    customer = Customer(
+        user_id=response.get('id'),
+        first_name=request.form.get('first_name'),
+        last_name=request.form.get('last_name')
+    )
+
+    try:
+        customer.save()
+    except:
+        return jsonify({
+            'error': 'Missing one or more field'
+        })
+
+    data = {**customer.serialize()}
+    data['access_token'] = response.get('access_token')
+
+    return jsonify(data)
