a7803e3a17d65ed144961c0e779ce7f4285470c9
diwangs <27280516+diwangs@users.noreply.github.com>
2018-11-16 18:50:33 +0700

    Added order

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 from authentication_broker.app import *
 from balance_service.app import *
 from customer_service.app import *
-
+from order_service.app import *
 
 app = Flask(__name__)
 auth_broker_url = 'http://127.0.0.1:5000/'
@@ -17,6 +17,8 @@ balance_service_thread = threading.Thread(target=balance_app.run, args=[], kwarg
 customer_service_url = 'http://127.0.0.1:5002'
 customer_service_thread = threading.Thread(target=customer_app.run, args=[], kwargs={'host': '127.0.0.1', 'port': 5002})
 
+order_service_url = 'http://127.0.0.1:5004'
+order_service_thread = threading.Thread(target=order_app.run, args=[], kwargs={'host': '127.0.0.1', 'port': 5004})
 
 @app.route('/auth', methods=['GET', 'POST'])
 @app.route('/auth/<path:varargs>', methods=['GET', 'POST'])
@@ -44,9 +46,17 @@ def customer_proxy(varargs=''):
     else:
         return requests.post(customer_service_url + varargs, headers=request.headers, data=request.form).text
 
+@app.route('/order', methods=['GET', 'POST'])
+@app.route('/order/<path:varargs>', methods=['GET', 'POST'])
+def order_proxy(varargs=''):
+    if request.method == 'GET':
+        return requests.get(order_service_url + varargs, headers=request.headers, params=request.args).text
+    else:
+        return requests.post(order_service_url + varargs, headers=request.headers, data=request.form).text
 
 if __name__ == '__main__':
     auth_broker_thread.start()
     balance_service_thread.start()
     customer_service_thread.start()
+    order_service_thread.start()
     app.run(host='127.0.0.1', port=9999)
