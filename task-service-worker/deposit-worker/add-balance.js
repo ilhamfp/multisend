fa2787f9988150ca5a88f1c04d0445a372199e4d
diwangs <27280516+diwangs@users.noreply.github.com>
2018-11-21 16:15:30 +0700

    Changed deposit to JSON

--- task-service-worker/deposit-worker/add-balance.js
+++ task-service-worker/deposit-worker/add-balance.js
@@ -9,24 +9,56 @@ async function addBalance({ task, taskService }) {
 	processVariables.set('secret_key', secret_key);
 	processVariables.set('amount', amount);
 
-	var url = 'http://localhost:5006/?wsdl';
-	var args = {
-		request:{
-			secret_key : secret_key,
-			bank_detail : "23523423423",
-			payment_method : "BCA",
-			amount : amount
-		}
-	};
-	soap.createClient(url, function(err, client) {
-	    client.deposit(args, function(err, result) {
-	        console.log(JSON.stringify(result))
-	        current_balance = result.depositResult.current_balance;
-	        console.log("Current balance is added by: " + amount);
-	        console.log("Current balance is: " + current_balance);
-	        taskService.complete(task, processVariables, null);
-	    });
-	});
+	const headers = {
+        'Content-Type': 'application/json',
+        'Authorization' : secret_key
+    };
+
+    const options = {
+        url: BASE_URL  + "balance/deposit",
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify({
+                                "bank" : '23523423423',
+                                "payment_method" : 'BCA',
+                                "amount" : amount
+                            })
+    };
+
+    request(options, function(error, response, body) {
+        // console.log(JSON.parse(response.body))
+        if (response.statusCode === 200) {
+            response = JSON.parse(response.body)
+            if (!response.error) {
+                current_balance = response.balance
+                console.log("Current balance is: " + current_balance);
+                taskService.complete(task, processVariables, null);
+            } else {
+                console.log(response.error);
+            }
+        } else {
+            console.log("ERROR ADD BALANCE: " + response.statusCode);
+        }
+    });
+
+	// var url = 'http://localhost:5006/?wsdl';
+	// var args = {
+	// 	request:{
+	// 		secret_key : secret_key,
+	// 		bank_detail : "23523423423",
+	// 		payment_method : "BCA",
+	// 		amount : amount
+	// 	}
+	// };
+	// soap.createClient(url, function(err, client) {
+	//     client.deposit(args, function(err, result) {
+	//         console.log(JSON.stringify(result))
+	//         current_balance = result.depositResult.current_balance;
+	//         console.log("Current balance is added by: " + amount);
+	//         console.log("Current balance is: " + current_balance);
+	//         taskService.complete(task, processVariables, null);
+	//     });
+	// });
 	console.log("Lewat add-balance, gan!");
 }
 
