3fa371221070e0c7299cd2d473a119e11eb9290a
Ilham Firdausi Putra <ilhamfputra31@gmail.com>
2018-11-21 08:19:11 +0700

    change withdrawal worker from SOAP to REST

--- task-service-worker/withdrawal-worker/validate-withdrawal.js
+++ task-service-worker/withdrawal-worker/validate-withdrawal.js
@@ -9,27 +9,62 @@ async function validateWithdrawal({ task, taskService }) {
 	processVariables.set('secret_key', secret_key);
 	processVariables.set('amount', amount);
 
-	var url = 'http://localhost:5006/?wsdl';
-	var args = {
-		request:{
-			secret_key : 'vIywNgjTJTNwIHZCXyHTZgfBepWwCx'
-		}
+	const headers = {
+	    'Content-Type': 'application/json',
+	    'Authorization' : secret_key
 	};
-	soap.createClient(url, function(err, client) {
-	    client.get_balance(args, function(err, result) {
-	        // result = JSON.stringify(result);
-	        current_balance = result.get_balanceResult.current_balance
-	        console.log("Current balance is: "+current_balance)
-	        if (amount < 0 || current_balance < amount) {
-	        	processVariables.set('sufficient', false);
-	        	console.log("Invalid withdrawal")
+
+	const options = {
+	    url: BASE_URL  + "balance",
+	    method: 'GET',
+	    headers: headers
+	};
+
+	request(options, function(error, response, body) {
+	    // console.log(JSON.parse(response.body))
+	    if (response.statusCode === 200) {
+	        response = JSON.parse(response.body)
+	        if (!response.error) {
+	            current_balance = response.balance
+    	        console.log("Current balance is: "+current_balance)
+    	        if (amount < 0 || current_balance < amount) {
+    	        	processVariables.set('sufficient', false);
+    	        	console.log("Invalid withdrawal")
+    	        } else {
+    	        	processVariables.set('sufficient', true);
+    	        	console.log("Valid withdrawal")
+    	        }
+    	        taskService.complete(task, processVariables, null);
 	        } else {
-	        	processVariables.set('sufficient', true);
-	        	console.log("Valid withdrawal")
+	            console.log(response.error);
 	        }
-	        taskService.complete(task, processVariables, null);
-	    });
+	    } else {
+	        console.log("ERROR VALIDATE WITHDRAWAL: " + response.statusCode);
+	    }
 	});
+
+    // Komen di bawah merupakan peninggalan sebelum diganti ke REST
+	// var url = 'http://localhost:5006/?wsdl';
+	// var args = {
+	// 	request:{
+	// 		secret_key : 'vIywNgjTJTNwIHZCXyHTZgfBepWwCx'
+	// 	}
+	// };
+	// soap.createClient(url, function(err, client) {
+	//     client.get_balance(args, function(err, result) {
+	//         // result = JSON.stringify(result);
+	//         current_balance = result.get_balanceResult.current_balance
+	//         console.log("Current balance is: "+current_balance)
+	//         if (amount < 0 || current_balance < amount) {
+	//         	processVariables.set('sufficient', false);
+	//         	console.log("Invalid withdrawal")
+	//         } else {
+	//         	processVariables.set('sufficient', true);
+	//         	console.log("Valid withdrawal")
+	//         }
+	//         taskService.complete(task, processVariables, null);
+	//     });
+	// });
 }
 
 module.exports = validateWithdrawal;
